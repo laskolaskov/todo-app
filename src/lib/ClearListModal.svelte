399becf107e6c7dff232702126182c17efd7f350
Lasko Laskov <laskov.lasko@gmail.com>
2023-03-25 19:03:30 +0200

    forgot a file

--- src/lib/ClearListModal.svelte
+++ src/lib/ClearListModal.svelte
@@ -1,38 +1,40 @@
 <script lang="ts">
     import { currentList, todos } from "./stores";
 
-    export let showModal: boolean;
+    export let showClearListModal: boolean;
     let dialog: HTMLDialogElement;
 
     function clearList() {
         $todos.set($currentList, []);
-        $todos = $todos
+        $todos = $todos;
     }
 
-    $: if (dialog && showModal) dialog.showModal();
+    $: if (dialog && showClearListModal) dialog.showModal();
 </script>
 
 <dialog
     class="border-4 border-black rounded-xl"
     bind:this={dialog}
-    on:close={() => (showModal = false)}
+    on:close={() => (showClearListModal = false)}
     on:click|self={() => dialog.close()}
     on:keypress|self={() => null}
 >
     <div on:click|stopPropagation on:keypress|self={() => null}>
         <p class="text-center text-black bold text-3xl mb-5">Are you sure ?</p>
-        <button
-            class="float-right border-2 rounded-md border-blue-500 bg-blue-400 text-white bold p-2 hover:bg-blue-300"
-            on:click={() => dialog.close()}
-            on:keypress={() => null}>CANCEL</button
-        >
-        <button
-            class="border-2 rounded-md border-red-500 bg-red-400 text-white bold p-2 hover:bg-red-300"
-            on:click={() => {
-                clearList()
-                dialog.close()
-            }}
-            on:keypress={() => null}>CLEAR</button
-        >
+        <div class="flex justify-center gap-2">
+            <button
+                class="float-right border-2 rounded-md border-blue-500 bg-blue-400 text-white bold p-2 hover:bg-blue-300"
+                on:click={() => dialog.close()}
+                on:keypress={() => null}>CANCEL</button
+            >
+            <button
+                class="border-2 rounded-md border-red-500 bg-red-400 text-white bold p-2 hover:bg-red-300"
+                on:click={() => {
+                    clearList();
+                    dialog.close();
+                }}
+                on:keypress={() => null}>CLEAR</button
+            >
+        </div>
     </div>
 </dialog>
